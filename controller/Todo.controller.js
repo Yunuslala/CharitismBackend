e25d7abf6688c9933b99d13494ea1833c55befb6
Mohd Yunus <112767616+Yunuslala@users.noreply.github.com>
2024-01-03 10:03:30 +0530

    removed extra unwanted if else statments

--- controller/Todo.controller.js
+++ controller/Todo.controller.js
@@ -1,71 +1,71 @@
 const AsyncerrorHandler = require("../middleware/AsyncErrorHandler");
 const { ErrorHandler } = require("../utils/ErrorHandler");
-const {TodoModel}=require("../models/TodoModel");
+const { TodoModel } = require("../models/TodoModel");
 module.exports = {
   createTodo: AsyncerrorHandler(async (req, res, next) => {
-    const {UserId,Task}=req.body;
-    const saveTask=new TodoModel({UserId,Task});
+    const { UserId, Task } = req.body;
+    const saveTask = new TodoModel({ UserId, Task });
     await saveTask.save();
     return res.status(201).send({
-        success:true,
-        msg:"task has been created by user"
-    })
+      success: true,
+      msg: "task has been created by user",
+    });
   }),
   getAllTodo: AsyncerrorHandler(async (req, res, next) => {
-    const getAllTodo=await TodoModel.find().populate('UserId',"name");
-    if(getAllTodo.length==0){
-        return next(new ErrorHandler(404,"Todo lists does not exist"))
+    const getAllTodo = await TodoModel.find().populate("UserId", "name");
+    if (getAllTodo.length == 0) {
+      return next(new ErrorHandler(404, "Todo lists does not exist"));
     }
     return res.status(200).send({
-        success:true,
-        data:getAllTodo
-    })
-
+      success: true,
+      data: getAllTodo,
+    });
   }),
   UpdateUserTodoStatus: AsyncerrorHandler(async (req, res, next) => {
-    const {UserId}=req.body;
-    const {id}=req.params;
-    const isExist=await TodoModel.findOne({_id:id});
-    if(!isExist){
-        return next(new ErrorHandler(404,"todo does not exist"))
-    }else{
-        const checkAUthorization=await TodoModel.findOne({_id:id,UserId});
-        if(!checkAUthorization){
-        return next(new ErrorHandler(400,"you are not authorized to update this task status"))
+    const { UserId } = req.body;
+    const { id } = req.params;
+    const isExist = await TodoModel.findOne({ _id: id });
+    if (!isExist) return next(new ErrorHandler(404, "todo does not exist"));
+    const checkAUthorization = await TodoModel.findOne({ _id: id, UserId });
+    if (!checkAUthorization)
+      return next(
+        new ErrorHandler(
+          400,
+          "you are not authorized to update this task status"
+        )
+      );
 
-        }
-            const updateStatus=await TodoModel.findByIdAndUpdate({_id:id},{status:true},{new:true});
-            return res.status(201).send({
-                success:true,
-                msg:"task has been updated",
-                data:updateStatus
-            })
-        
-    }
-
-
-
+    const updateStatus = await TodoModel.findByIdAndUpdate(
+      { _id: id },
+      { status: true },
+      { new: true }
+    );
+    return res.status(201).send({
+      success: true,
+      msg: "task has been updated",
+      data: updateStatus,
+    });
   }),
   removeUserTodo: AsyncerrorHandler(async (req, res, next) => {
-    const {UserId}=req.body;
-    const {id}=req.params;
-    const isExist=await TodoModel.findOne({_id:id});
-    if(!isExist){
-        return next(new ErrorHandler(404,"todo does not exist"))
-    }else{
-        const checkAUthorization=await TodoModel.findOne({_id:id,UserId});
-        if(!checkAUthorization){
-        return next(new ErrorHandler(400,"you are not authorized to delete this task "))
+    const { UserId } = req.body;
+    const { id } = req.params;
+    const isExist = await TodoModel.findOne({ _id: id });
+    if (!isExist) return next(new ErrorHandler(404, "todo does not exist"));
 
-        }
-            const updateStatus=await TodoModel.findByIdAndDelete({_id:id},{status:true},{new:true});
-            return res.status(201).send({
-                success:true,
-                msg:"task status has been deleted"
-            })
-        
-    }
+    const checkAUthorization = await TodoModel.findOne({ _id: id, UserId });
+    if (!checkAUthorization)
+      return next(
+        new ErrorHandler(400, "you are not authorized to delete this task ")
+      );
 
-
+    const updateStatus = await TodoModel.findByIdAndDelete(
+      { _id: id },
+      { status: true },
+      { new: true }
+    );
+    return res.status(201).send({
+      success: true,
+      msg: "task status has been deleted",
+    });
   }),
 };
